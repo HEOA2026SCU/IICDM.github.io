a828a56a506d7576a0004e58e7ddde4c84fa66f3
HEOA2026SCU <hylai6818@scu.edu.cn>
2026-02-02 17:10:29 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // 示例：研究动态的链接列表
 const newsLinks = [
-    { title: "会议纪要1", url: "https://example.com/meeting1" },
-    { title: "调研记录2", url: "https://example.com/survey2" },
-    { title: "新闻发布3", url: "https://example.com/news3" }
+    { title: "2025年9月26日 国家科技重大专项课题“整合式智慧慢病管理模式研究”启动会顺利举行", url: "https://mp.weixin.qq.com/s/zj19z1DcZdE7bgmVevohcA" },
+    { title: "2026年1月29日 国家科技重大专项课题“整合式智慧慢病管理模式研究”年度研讨会顺利举行", url: "https://mp.weixin.qq.com/s/RPzWc1Bv88lnvJsBbUJ32Q" },
+    { title: "持续更新中", url: "https://example.com/news3" }
 ];
 
 // 动态填充研究动态链接
@@ -18,8 +18,8 @@ newsLinks.forEach(link => {
 
 // 示例：研究成果的文档列表
 const documents = [
-    { title: "研究报告1", url: "https://example.com/report1" },
-    { title: "研究成果2", url: "https://example.com/result2" }
+    { title: "研究报告1：《柳叶刀-西太平洋》期刊发表中国版门急诊服务敏感疾病目录", url: "https://doi.org/10.1016/j.lanwpc.2025.101734" },
+    { title: "研究成果2：《四川大学学报（医学版）》期刊发表四川省德阳市罗江区医防融合创新实践分析", url: "https://kns.cnki.net/kcms2/article/abstract?v=K6QWI_fIKJGir1Cf616maST8yv5y8ga_CbAan4CxzRyrZIwI226P_Q8PgIjQ0693acveMf43okkkfLj0kY-uWqnXoS5Vg-MCSyCLrNxtPq5oCtHQQn9ofytmPIC5pfI-OlPbLlxtzoE5yKq35UQkc6XBdjqaf6Hl1S5ouRA8VYY=&uniplatform=NZKPT" }
 ];
 
 // 动态填充研究成果文档
